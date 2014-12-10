a589181af6321ac10d0959ff2a75d4fb6eb42290
David Tomandl <davidtomandl@users.noreply.github.com>
2014-12-10 07:21:41 +0100

    Update BipartiteMatching.cpp

--- src/BipartiteMatching.cpp
+++ src/BipartiteMatching.cpp
@@ -74,7 +74,7 @@ void BipartiteMatching::readGraphList(std::istream& stream)
 	init();
 	size_t u,v;
 	while(!stream.eof())
-    {
+        {
 		stream >> u >> v;
 		graph_.set(u,v,1);
 		graph_.set(v,u,1);
@@ -90,7 +90,7 @@ void BipartiteMatching::readGraphMatrix(std::istream& stream)
 	init();
 	
 	size_t temp;
-    for (size_t i = 0; i < n_vertices_; ++i) 
+        for (size_t i = 0; i < n_vertices_; ++i) 
 		for (size_t j = 0; j < n_vertices_; ++j) 
 		{
 			stream >> temp;
@@ -113,23 +113,23 @@ void BipartiteMatching::init()
 bool BipartiteMatching::setPartite()
 {
 	std::queue <size_t> q;//FIFO of vertex numbers
-    q.push(0);
+        q.push(0);
 	partite_[0]=0;
 
 	while (!q.empty())
-    {
-		size_t u = q.front();
-        q.pop();
-		// Find all unused adjacent vertices
-        for (size_t v = 0; v < n_vertices_; v++)
         {
+		size_t u = q.front();
+                q.pop();
+		// Find all unused adjacent vertices
+        	for (size_t v = 0; v < n_vertices_; v++)
+	        {
 			if (graph_.at(u,v)!=0 &&  partite_[v]==-1)
-            {                
+        	        {                
 				partite_[v] = 1 - partite_[u];//set opposite partite
-                q.push(v);
-            }
+                		q.push(v);
+        	        }
 			else if (graph_.at(u,v) && partite_[v] == partite_[u])
-                return false;
+        			 return false;
 		}
 		if (q.empty())
 			for (size_t i = 0; i <n_vertices_; i++)
